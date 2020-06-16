fd0e7ab421571b55346f12b6550841f51b2cc70b
Necromancer5211 <genuine.basilnt@gmail.com>
2020-06-16 15:46:34 +0530

    updated menus to navigate with withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import {Switch,Route} from 'react-router-dom';
 import HomePage from "./Pages/Homepage/homepage.component";
 
+const Hats = () => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+);
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+        <switch>
+            <Route exact path="/" component={HomePage}/>
+            <Route exact path="/shop/hats" component={Hats}/>
+        </switch>
     </div>
   );
 }
